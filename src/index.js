35c27f2fb6698eed7b7b9b1497b779698a8b6174
Blake Peterson <121067208+Blake-Peterson@users.noreply.github.com>
2024-11-04 17:54:01 -0600

    remove greetings.js and added fetch function in index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
-import {greeting} from "./greeting.js";
 import "./styles.css";
 
-console.log(greeting);
+const location_name = "London";
+//best practices are for the key not to be in frontend, but is free key with limited uses
+const visual_crossing_api_key = '25HD5UH4UEA3U9AQGBRZQ2SEG';
+
+const url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'+ location_name +'?key=' +visual_crossing_api_key;
+
+async function fetchLocationData(){
+    try{
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+    } catch(error){
+        console.log(error);
+    }
+}
+
+function main(){
+    fetchLocationData();
+}
+
+main();
